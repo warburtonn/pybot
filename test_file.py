d81de1dd55ebc73a6330eff0cd2cf51180a86247
sergey <lsnakemanl@gmail.com>
2018-12-27 14:26:32 +0200

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,10 +1,9 @@
 ***Variables***
-# variable      card_number         date    cvv
-@{card}         4444555566661111    0119    123
-${card_part}
+# variable             card_number         date    cvv
+@{credit_card}         4444555566661111    0119    123
 
 ***Test Cases ***                   amount              receiver_card
-test1                               100                 @{card}
+test1                               100                 @{credit_card}
 
 *** Keywords ***
 [Arguments]          @{card}
@@ -14,8 +13,10 @@ Input Card Number With FOR
 
 ----------------------
 Another file.txt
-Input Card Number With FOR    @{card}  
-    [Arguments]         @{card}
-        :FOR            ${card_part}    IN      @{сard}[0]
-        Focus   ${FIELD_LOCATOR}
-        Input text     ${FIELD_LOCATOR}     ${card_part}
+Input Card Number with for
+     [Arguments]         @{credit_card}
+     Wait until element is visible    ${NEW_P2P_SENDER_CARD}
+     ${card_mumber_1}=       Get slice from list     @{credit_card}[0]     0       15
+        :FOR    ${Item}   IN    ${credit_card_1}
+#         \      press key       ${NEW_P2P_SENDER_CARD}      ${card_mumber_1}
+          \     input text      ${NEW_P2P_SENDER_CARD}       ${card_number_1}
